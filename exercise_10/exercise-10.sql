a150b9823e783314589c1a8b7451267f5872fb72
Dillon Brock <mdillon1297@gmail.com>
2022-08-02 12:23:54 -0700

    exercise 10 passing test

--- exercise_10/exercise-10.sql
+++ exercise_10/exercise-10.sql
@@ -2,4 +2,8 @@
 -- Similar to exercise 9 with an additonal join
 
 SELECT name FROM category
-INNER JOIN 
+LEFT JOIN film_category ON film_category.category_id = category.category_id
+LEFT JOIN film ON film.film_id = film_category.film_id
+INNER JOIN inventory ON inventory.film_id = film.film_id
+LEFT JOIN rental ON rental.inventory_id = inventory.inventory_id
+WHERE rental_date IS NULL;
