219041f06a73372be098256592893538f31b066a
Dillon Brock <mdillon1297@gmail.com>
2022-08-02 14:11:41 -0700

    exercise 16 and all other tests passing

--- exercise_16/exercise_16.sql
+++ exercise_16/exercise_16.sql
@@ -1,3 +1,9 @@
 -- return the title, description, release year and list of actors returned as a JSON blob
 -- for the movie Airplane Sierra
-    
+
+SELECT film.title, film.description, film.release_year,
+COALESCE(json_agg(to_jsonb(actor.*))) as actors from film
+LEFT JOIN film_actor ON film_actor.film_id = film.film_id
+LEFT JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE film.title = 'Airplane Sierra'
+GROUP BY film.title, film.description, film.release_year;
